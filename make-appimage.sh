f9465a65a7f481feacef15ce5b8fc2fd874f930a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-10 19:37:48 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/gitbutler-tauri.png
+export DESKTOP=/usr/share/applications/GitButler.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_P11KIT=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/gitbutler*
 
 # Additional changes can be done in between here
 
